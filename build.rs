5aeec186c797b4ce048b2a60a6d2c33f4253cc9d
elsid <elsid.mail@gmail.com>
2015-09-06 21:31:35 +0300

    Use absolute paths in build script

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() {
+    use std::path::Path;
     use std::process::Command;
-    Command::new("cmake").args(&["."]).current_dir("bobyqa-cpp").status().unwrap();
-    Command::new("make").current_dir("bobyqa-cpp").status().unwrap();
-    println!("cargo:rustc-link-search=native=bobyqa-cpp/lib");
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("bobyqa-cpp");
+    Command::new("cmake").args(&["."]).current_dir(&dir).status().unwrap();
+    Command::new("make").current_dir(&dir).status().unwrap();
+    println!("cargo:rustc-link-search=native={}", dir.join("lib").display());
     println!("cargo:rustc-link-lib=static=bobyqa");
 }
